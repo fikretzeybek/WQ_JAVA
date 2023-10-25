854961fc71cfd239462c643819f0a4e6cc532509
fikretzeybek <fzeybek1972@gmail.com>
2023-10-25 23:35:59 +0300

    f33

---
+++ src/F33_MapUpdate_EntryKullanimi/F10_BookingApi.java
@@ -0,0 +1,26 @@
+package F33_MapUpdate_EntryKullanimi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class F10_BookingApi {
+
+    public static void main(String[] args) {
+
+        /*
+
+         {
+            "firstname" : "Ahmet",
+            "lastname" : "Bulut",
+            "totalprice" : 500,
+            "depositpaid" : false,
+            "bookingdates" : {
+                   "checkin" : "2023-07-21",
+                   "checkout" : "2023-08-10"
+                      },
+            "additionalneeds" : "wi-fi" }
+
+         */
+        Map<String, Object> bookingMap = new HashMap<>();
+    }
+}
